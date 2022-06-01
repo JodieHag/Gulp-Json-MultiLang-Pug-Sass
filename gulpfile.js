8651d71a27943ef5068085e9794e3ebaaa0db787
Judit Sánchez <judit@jelly-brains.com>
2022-06-01 21:50:39 +0200

    update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,15 @@ const livereload = require('gulp-livereload')
 const log = require('fancy-log')
 const merge = require('merge-stream')
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss')
+const imagemin = require('gulp-imagemin')
 const postcss = require('gulp-postcss')
 const open = require('gulp-open')
 const prettier = require('gulp-prettier')
 const pug = require('gulp-pug')
 const rename = require('gulp-rename')
 const zip = require('gulp-zip')
+const webpack = require('webpack-stream')
 
 // importar configuración de idioma y páginas
 const config = require('./config')
@@ -44,6 +47,17 @@ gulp.task('css', () => {
   return gulp
     .src(['dev/css/**/*.*'])
     .pipe(postcss(plugins))
+    .pipe(
+      purgecss({
+        content: ['dev/**/*.{html,pug,js}'],
+        defaultExtractor: (content) => {
+          const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+          const innerMatches =
+            content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+          return broadMatches.concat(innerMatches)
+        },
+      })
+    )
     .pipe(gulp.dest('www/css'))
     .pipe(livereload())
     .on('end', () => {
@@ -73,16 +87,25 @@ gulp.task('sass', async () => {
 
 // Concatenate & Minify JS
 gulp.task('scripts', () => {
+  for (const page in projectSettings.pages) {
   return gulp
-    .src(['node_modules/babel-polyfill/dist/polyfill.js', 'src/js/*.js'])
+    .src(['node_modules/babel-polyfill/dist/polyfill.js', `dev/js/pages/${page}.js`])
     .pipe(prettier())
-    .pipe(babel())
-    .pipe(concat('main.js'))
+    .pipe(
+      webpack({
+        mode: 'production',
+        module: {
+          rules: [{ test: /\.js$/, use: 'babel-loader' }],
+        },
+      })
+    )
+    .pipe(concat(`${page}.js`))
     .pipe(gulp.dest('www/js'))
     .pipe(livereload())
     .on('end', () => {
       log('scripts Done!')
     })
+  }
 })
 
 // copy statics && external css
@@ -93,6 +116,17 @@ gulp.task('statics', () => {
     .pipe(livereload())
 
   return merge(media)
+})
+
+
+gulp.task('imageOptimized', async () => {
+  const img = await gulp
+    .src('dev/media/img/**/*.*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('www/media/img'))
+    .pipe(livereload())
+
+  return merge(img)
 })
 
 // compile pug to html and translate
@@ -110,6 +144,7 @@ gulp.task('pug', async () => {
                   projectSettings.langs[lang] +
                   '/translation.json'),
                 lang: lang,
+                page: page,
               },
             })
           )
@@ -141,6 +176,25 @@ gulp.task('pug', async () => {
       )
     }
   }
+  await gulp
+    .src('dev/pug/pages/index.pug')
+    .pipe(
+      pug({
+        locals: {
+          i18n: require('./dev/i18n/en-GB/translation.json'),
+          lang: 'en',
+          page: 'index',
+        },
+      })
+    )
+    .pipe(
+      rename({
+        basename: 'index',
+      })
+    )
+    .on('error', (error) => log(error))
+    .pipe(gulp.dest(`www`))
+    .pipe(livereload());
 })
 
 // Open one file with default application
@@ -179,9 +233,15 @@ gulp.task('watch', async () => {
   await gulp.watch('dev/js/vendor/**/*.js', gulp.series(['vendor']))
   await gulp.watch('dev/js/*.js', gulp.series(['scripts']))
   await gulp.watch('dev/js/components/*.js', gulp.series(['scripts']))
+  await gulp.watch('dev/js/molecules/*.js', gulp.series(['scripts']))
   await gulp.watch('dev/sass/*/*.*', gulp.series(['sass']))
+  await gulp.watch('dev/sass/**/*.*', gulp.series(['sass']))
+  await gulp.watch('dev/sass/**/**/*.*', gulp.series(['sass']))
+  await gulp.watch(['dev/i18n/**/*.json'], gulp.series(['pug']))
   await gulp.watch(['dev/pug/*.pug', 'dev/pug/**/*.pug'], gulp.series(['pug']))
-  await gulp.watch('dev/pug/partials/*.pug', gulp.series(['pug']))
+  await gulp.watch('dev/pug/**/*.pug', gulp.series(['pug']))
+  await gulp.watch('dev/pug/sections/**/*.pug', gulp.series(['pug']))
+  await gulp.watch('dev/pug/includers/*.pug', gulp.series(['pug']))
   for (const lang in projectSettings.langs) {
     await gulp.watch(
       `dev/i18n/${projectSettings.langs[lang]}/*.json`,
@@ -201,7 +261,7 @@ gulp.task('zip', () => {
 gulp.task('styles', gulp.series(['sass', 'css']))
 gulp.task(
   'scripts-statics',
-  gulp.parallel(['statics', 'scripts', 'vendor', 'json'])
+  gulp.parallel(['statics', 'scripts', 'vendor', 'imageOptimized', 'json'])
 )
 gulp.task('server', gulp.series(['connect', 'watch']))
 gulp.task('build', gulp.series(['styles', 'scripts-statics', 'pug']))
